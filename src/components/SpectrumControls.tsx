13a70b4cc1f03b36b4ba38e15fd053b45f9ad4bb
Nick <nickgcs7@gmail.com>
2026-04-14 11:51:44 -0700

    Delete src/components/SpectrumControls.tsx

--- src/components/SpectrumControls.tsx
+++
@@ -1,6 +0,0 @@
-// DEPRECATED: This component is no longer used.
-// The Spectrum Analyzer visualization has been removed.
-// This file can be safely deleted.
-export default function SpectrumControls(_props: { visible: boolean; onClose: () => void }) {
-  return null
-}
